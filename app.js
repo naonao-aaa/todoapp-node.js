735be34b67fc4eb369de063567a22ed0abde3c43
naonao-aaa <naonao1814naonao@gmail.com>
2022-11-01 22:22:22 +0900

    Node.jsとMongoDBを接続

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const app = express();
 const taskRoute = require("./routes/tasks");
+const connectDB = require("./db/connect");
 
 const PORT = 5000;
 
 // ルーティング設計
 app.use("/api/v1/tasks", taskRoute);
 
-app.listen(PORT, console.log("サーバーが起動しました"));
+// データベースと接続　　　　　　　　　　
+const start = async () => {
+  try {
+    await connectDB("mongodb+srv://udemy:abc@cluster0.hpeeb2z.mongodb.net/todoapp?retryWrites=true&w=majority");
+    app.listen(PORT, console.log("サーバーが起動しました"));
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+start();
