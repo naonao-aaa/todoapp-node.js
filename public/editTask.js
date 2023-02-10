ca5e9bf6009a0f704b88258eaa58d9db8b403bb9
naonao-aaa <naonao1814naonao@gmail.com>
2023-02-10 15:52:30 +0900

    完了・未完了状態を編集

--- public/editTask.js
+++ public/editTask.js
@@ -2,6 +2,7 @@ const taskIDDOM = document.querySelector(".task-edit-id");
 const taskNameDOM = document.querySelector(".task-edit-name");
 const editFormDOM = document.querySelector(".single-task-form");
 const formAlertDOM = document.querySelector(".form-alert");
+const taskCompletedDOM = document.querySelector(".task-edit-completed");
 
 const params = window.location.search;
 const id = new URLSearchParams(params).get("id");
@@ -27,8 +28,10 @@ editFormDOM.addEventListener("submit", async (e) => {
   e.preventDefault();
   try {
     const taskName = taskNameDOM.value;
+    const taskCompleted = taskCompletedDOM.checked;
     const {data: task} = await axios.patch(`/api/v1/tasks/${id}`, {
-      name: taskName
+      name: taskName,
+      completed: taskCompleted
     });
     formAlertDOM.style.display = "block";
     formAlertDOM.textContent = "編集に成功しました";
